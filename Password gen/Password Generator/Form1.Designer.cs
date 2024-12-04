ea2f138cabbd2c717c96966ec8b5c6e09942b6db
MichaelJohnCup <jancalek225@gmail.com>
2024-12-04 11:59:42 +0100

    Add files via upload
    Uploading files

---
+++ Password gen/Password Generator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Password_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(127, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(439, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(160, 148);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(108, 19);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Weak Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(160, 173);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(124, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Medium Password";
+            checkBox2.TextAlign = ContentAlignment.MiddleCenter;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(160, 198);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(114, 19);
+            checkBox3.TabIndex = 1;
+            checkBox3.Text = "Strong Password";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(160, 251);
+            button1.Name = "button1";
+            button1.Size = new Size(314, 50);
+            button1.TabIndex = 2;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(244, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Password Generator";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(685, 426);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ Password gen/Password Generator/Form1.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics.Eventing.Reader;
+using System.Text;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked) 
+            {
+                PassGen(6);
+            }
+            else if (checkBox2.Checked) 
+            {
+                PassGen(12);
+            }
+            else if(checkBox3.Checked) 
+            {
+                PassGen(18);
+            }
+            else
+            {
+                PassGen(8);
+            }
+
+        }
+        public void PassGen(int len)
+        {
+            const string ValidChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ1234567890";
+            System.Text.StringBuilder result = new StringBuilder();
+            Random random = new Random();
+            while (0 < len--)
+            {
+                result.Append(ValidChar[random.Next(ValidChar.Length)]);
+            }
+            textBox1.Text = result.ToString();
+        }
+    }
+}
